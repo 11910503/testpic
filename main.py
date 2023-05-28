c0d2fc34ba472b9b2bb27e7cbc350190fb07445d
11910503 <64644882+11910503@users.noreply.github.com>
2023-05-28 17:18:48 +0800

    0.0.5

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cv2
 
 
 
-cap=cv2.VideoCapture(0)
+cap=cv2.VideoCapture("nvarguscamerasrc ! video/x-raw(memory:NVMM), width=1280, height=720, format=NV12, framerate=30/1 ! nvvidconv  ! video/x-raw, width=1280, height=720, format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink")
 if cap.isOpened():
     pass
 index,image=cap.read()
